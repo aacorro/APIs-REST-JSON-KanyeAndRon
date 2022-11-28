2e66abe4d2de46f9a3f375bdf50901d3821c9947
Abdiel Corro <acorrop@outlook.com>
2022-11-28 11:37:51 -0600

    exercise completed

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using APIs_JSON_KanyeAndRon;
+
+var client = new HttpClient();
+
+var quote = new QuoteGenerator(client);
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Kanye: {quote.Kanye()}");
+    Console.WriteLine($"Ron: {quote.Ron()}");
+
+    Console.WriteLine("----------------------------------------------------");
+
+}
---
+++ QuoteGenerator.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace APIs_JSON_KanyeAndRon
+{
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;   
+        }
+
+        public string Kanye()
+        {
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string Ron()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+}
